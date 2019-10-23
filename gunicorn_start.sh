7a0bd0305b478a1df2121d10218e86872fb00535
root <root@ip-172-31-3-144.sa-east-1.compute.internal>
2019-10-23 03:57:30 +0000

    gunicorn stuff

---
+++ gunicorn_start.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+service xinetd stop
+service nginx restart
+
+NAME="achilles"
+FLASK_DIR=/var/www/achilles/src            	# Flask project directory
+SOCKFILE=/var/www/achilles/gunicorn.sock	# we will communicte using this unix socket
+USER=root                               	# the user to run as
+GROUP=root                                     	# the group to run as
+NUM_WORKERS=1                                 	# how many worker processes should Gunicorn spawn
+FLASK_WSGI_MODULE=achilles.wsgi              	# WSGI module name
+
+FIREBASE_CREDENTIALS=/var/www/achilles/src/achilles/achilles-pj-dev-fdc87e41fbbd.json
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+
+cd $FLASK_DIR
+source /var/www/achilles/bin/activate
+export FIREBASE_CREDENTIALS=$FIREBASE_CREDENTIALS
+export PYTHONPATH=$FLASK_DIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+# RUNDIR=$(dirname $SOCKFILE)
+# test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+
+exec /var/www/achilles/bin/gunicorn ${FLASK_WSGI_MODULE}:myapp \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER \
+  --group=$GROUP \
+  --bind=unix:$SOCKFILE \
+  --access-logfile -
